ac9563b83fd0c2926951e1bf9d2cd4f16318dad7
Tornado3708 <sami.vagebi@gmail.com>
2024-12-15 19:40:02 +0200

    commit

--- src/buffers.ts
+++ src/buffers.ts
@@ -1,8 +1,8 @@
 export const 
-  x      = new WeakMap<Record<any, number>>(),
-  y      = new WeakMap<Record<any, number>>(),
-  z      = new WeakMap<Record<any, number>>(),
-  radius = new WeakMap<Record<any, number>>(),
-  width  = new WeakMap<Record<any, number>>(),
-  height = new WeakMap<Record<any, number>>(),
-  values = new WeakMap<Record<any, Float64Array>>();
+  x      = new WeakMap(),
+  y      = new WeakMap(),
+  z      = new WeakMap(),
+  radius = new WeakMap(),
+  width  = new WeakMap(),
+  height = new WeakMap(),
+  values = new WeakMap();
